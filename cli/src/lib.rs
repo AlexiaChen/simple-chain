625b272562c859faf7c1f83637770f22f7ed84a2
MathxH Chen <brainfvck@foxmail.com>
2021-03-22 19:17:44 +0800

    Update cli opt

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -2,6 +2,7 @@ extern crate app;
 
 #[allow(unused_imports)]
 use app::{App, Args, Cmd, Opt, OptTypo, OptValue, OptValueParse};
+use std::env;
 pub struct Cli {}
 
 impl Cli {
@@ -20,12 +21,19 @@ pub struct Config {
     pub balance_addr: String,
     pub list_addr_limit: usize,
     pub miner_addr: String,
+    pub node_id: String,
+    pub send_tx_from: String,
+    pub send_tx_to: String,
+    pub send_tx_amount: Vec<i32>,
+    pub get_block_height: u32,
+    pub get_block_hash: String,
+    pub get_tx_id: Vec<String>,
 }
 
 impl Config {
     fn parse_args() {
         let mut config = Config::default();
-
+        config.node_id = env::var("NODE_ID").unwrap_or("null".to_string());
         let helper = {
             App::new("simplechain")
                 .version("0.0.1")
@@ -68,15 +76,57 @@ impl Config {
                 )
                 .cmd(
                     Cmd::new("getblock")
+                    .desc("get block info by height or hash")
+                    .opt(
+                        Opt::new("height", &mut config.get_block_height)
+                        .short('h')
+                        .long("height")
+                        .help("block height")
+                    )
+                    .opt(
+                        Opt::new("hash", &mut config.get_block_hash)
+                        .short('i')
+                        .long("hash")
+                        .help("block hash")
+                    )
                 )
                 .cmd(
                     Cmd::new("gettx")
+                    .desc("get transaction info by tx hash")
+                    .args(
+                        Args::new("TXID", &mut config.get_tx_id)
+                        .help("tx id or tx hash")
+                    )
+
                 )
                 .cmd(
                     Cmd::new("sendtx")
+                    .desc("send tx from address to another address")
+                    .opt(
+                        Opt::new("from", &mut config.send_tx_from)
+                        .short('f')
+                        .long("from")
+                        .help("tx from address")
+                    )
+                    .opt(
+                        Opt::new("to", &mut config.send_tx_to)
+                        .short('t')
+                        .long("to")
+                        .help("tx to address")
+                    )
+                    .args(
+                        Args::new("AMOUNT", &mut config.send_tx_amount)
+                        .help("token amount")
+                    )
                 )
                 .cmd(
                     Cmd::new("startnode")
+                    .desc("start node server")
+                    .opt(
+                        Opt::new("miner", &mut config.miner_addr)
+                        .long("miner")
+                        .help("miner address")
+                    )
                 )
                 .parse_args()
         };
